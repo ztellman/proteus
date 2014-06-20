0de1035c48f537bd22c7cae12e896307579400b0
Colin Jones <trptcolin@gmail.com>
2014-06-19 21:03:14 -0500

    Bump to latest riddley (0.1.7)
    letfn isn't having its locals registered as part of the letfn*
    transformation, so its bindings weren't visible to the analyzer inside
    nested let expressions. So swallowing analysis errors as riddley
    0.1.7 gets this use case working, but the right fix is probably to
    register locals for letfn.
    
    refs #3

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
   :description "local. mutable. variables."
   :license {:name "MIT License"
             :url "http://opensource.org/licenses/MIT"}
-  :dependencies [[riddley "0.1.4"]]
+  :dependencies [[riddley "0.1.7"]]
   :profiles {:dev {:dependencies [[org.clojure/clojure "1.5.1"]
                                   [criterium "0.4.1"]]}}
   :java-source-paths ["src"]
