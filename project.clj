e56a8eccd0715e874c021fdbf0812dd40b017972
Zach Tellman <ztellman@gmail.com>
2016-01-05 23:56:45 -0800

    target latest riddley, mark 0.1.6

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject proteus "0.1.5"
+(defproject proteus "0.1.6"
   :description "local. mutable. variables."
   :license {:name "MIT License"
             :url "http://opensource.org/licenses/MIT"}
-  :dependencies [[riddley "0.1.11"]]
-  :profiles {:dev {:dependencies [[org.clojure/clojure "1.8.0-RC2"]
+  :dependencies [[riddley "0.1.12"]]
+  :profiles {:dev {:dependencies [[org.clojure/clojure "1.8.0-RC4"]
                                   [criterium "0.4.1"]]}}
   :java-source-paths ["src"]
   :javac-options ["-target" "1.5" "-source" "1.5"]
